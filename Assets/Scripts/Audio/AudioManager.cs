a6e8736d1f6e278151e8042c3c7c388dabc858fb
olivermena-ae <olivermena.ae@gmail.com>
2026-02-07 19:42:24 -0600

    AudioManager
    Implementation of AudioManager script and Audio GameObject

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UIElements;
+
+public class AudioManager : MonoBehaviour
+{
+public static AudioManager master {get; private set;}
+public MXManager MX { get; private set; }
+public SFXManager SFX { get; private set; }
+
+
+[Header("Mixers")]
+[SerializeField] private AudioMixerGroup mXMixer;
+public AudioMixerGroup MXMixer => mXMixer;
+[SerializeField] private AudioMixerGroup sFXMixer;
+public AudioMixerGroup SFXMixer => sFXMixer;
+
+
+
+[Header("Audio Source Pool")]
+[SerializeField] private int poolSize = 10;
+public int PoolSize => poolSize;
+
+
+    private void Awake()
+    {
+        if (master != null && master != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        master = this;
+        DontDestroyOnLoad(gameObject);
+
+        MX = GetComponent<MXManager>();
+        SFX = GetComponent<SFXManager>();
+    }
+
+}
---
+++ Assets/Scripts/Audio/MXManager.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MXManager : MonoBehaviour
+{
+
+}
